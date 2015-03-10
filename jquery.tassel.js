f8fd2982240b758edf09ec4b1f2c525b82dbe78e
Kumsal Obuz <info@viroidgames.com>
2015-03-10 16:24:51 -0400

    Repetition logic for repeating the middle part

--- jquery.tassel.js
+++ jquery.tassel.js
@@ -1,5 +1,6 @@
 /*jshint jquery: true*/
 $.fn.tassel = function (options) {
+    'use strict';
 
 	options = $.extend({}, options);
 
@@ -22,7 +23,7 @@ $.fn.tassel = function (options) {
 	var hr = this;
 
 	//this will be called only when the last image is loaded which means we either have 2 or 3 images
-	var _layout = function () {
+	function layout(event) {
 
 		//necessary part to construct background image reference, wrap with url()
 		options.tassels.forEach(function (element, index, array) {
@@ -31,21 +32,37 @@ $.fn.tassel = function (options) {
 
 		//depending on having 2 or 3 images for the hr we are starting to decorate
 		hr.css({
-				"background-image": options.tassels.join(","),
-				"background-position": "top left, top right" + (options.tassels.length == 3 ? ", top center" : ""),
-				"background-repeat": "no-repeat, no-repeat" + (options.tassels.length == 3 ? ", repeat-x" : ""),
-				"background-clip": "padding-box, padding-box" + (options.tassels.length == 3 ? ", content-box" : ""),
-				"padding": "0 " + options.tassel_width + "px",
-				"height": options.tassel_height + "px",
-				"border": "0",
-				"width": (options.tassels.length === 2 ? "0" : "initial")
-			})
+			"background-image": options.tassels.join(","),
+			"background-position": "top left, top right" + (options.tassels.length == 3 ? ", 0% 10%" : ""),
+			"background-repeat": "no-repeat, no-repeat" + (options.tassels.length == 3 ? ", repeat-x" : ""),
+			"background-clip": "padding-box, padding-box" + (options.tassels.length == 3 ? ", content-box" : ""),
+			"padding": "0 " + options.tasselWidth + "px",
+			"height": options.tasselHeight + "px",
+			"border": "0"
+		})
 			.addClass('tasselled');
 
-	};
+		if(parseInt(options.repetition, 10) > 0){
+			hr.width(options.repetition * options.repetitionWidth);
+		}
+		else if (parseInt(options.repetition, 10) === -1){
+			hr.width(availableWidth);
+		}
+	}
+
+	function availableWidth(){
+		//we can't always assume hr's parent will have width value as the perfect multiplier of 2 * options.tasselWidth + options.repetition * options.repetitionWidth
+		//since tasselWidth is already added to the component through padding-box we have to subtract it from parent width
+		var componentWidth = hr.parent().width() - 2 * options.tasselWidth;
+		//then the remaining space may not necesarily divide by repetitionWidth evenly so let's find the excess
+		var excess = componentWidth % options.repetitionWidth;
+		//finally the available widht is what's left from componentWidth - excess to add up to a round multiplier of options.repetition * options.repetitionWidth
+		return componentWidth - excess;
+	}
 
 	return this.each(function () {
 
+		var numOfLoadedTessels = 0;
 
 		/*
 		each image will be loaded so they can later be used as background image to decorate hr
@@ -56,27 +73,34 @@ $.fn.tassel = function (options) {
 
 			$('<img/>')
 				.load(function (event) {
+				numOfLoadedTessels++;
 
-					if (index === 0) {
-						options.tassel_width = event.currentTarget.width;
-						options.tassel_height = event.currentTarget.height;
-					}
+				//first indexed image carries enough information to register width and heigth of the outermost tassels
+				if (index === 0) {
+					options.tasselWidth = event.currentTarget.width;
+					options.tasselHeight = event.currentTarget.height;
+				}
 
-					// we want to layout only when last image has been loaded
-					if (index === options.tassels.length - 1) {
-						_layout(event);
-					}
+				//if index is 2 that means we have middle tessel as well
+				if(index === 2){
+					options.repetitionWidth = event.currentTarget.width;
+				}
+
+				// we want to layout only when last image has been loaded whether it's the middle or the right piece
+				if(numOfLoadedTessels===options.tassels.length){
+					layout(event);
+				}
 
 
-				})
+			})
 				.error(function (event) {
-					hr.trigger({
-						type: "tasselImageLoadError",
-						tassel: index === 0 ? "left" : (index === 2 ? "middle" : "right"),
-						path: event.currentTarget.src
-					});
+				hr.trigger({
+					type: "tasselImageLoadError",
+					tassel: index === 0 ? "left" : (index === 2 ? "middle" : "right"),
+					path: event.currentTarget.src
+				});
 
-				})
+			})
 				.attr('src', element);
 
 
